298ef77ed4c476d64edb0aeb954c33bbd527370d
VasaPopkin <nmichkarev@mail.ru>
2016-09-26 15:17:20 +0800

    compile

---
+++ www/js/index.js
@@ -0,0 +1,94 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        if(navigator.connection.type == 'none') {
+            var element = document.getElementsByClassName('received');
+            element = element[0];
+            if (element) element.style.display = 'block';
+            var checkConnect = setInterval(function() {
+                console.log(navigator.connection.type);
+                if(navigator.connection.type != 'none') {
+                    clearInterval(checkConnect);
+                    app.onDeviceConnected();
+                }
+            }, 400);
+        } else {
+            app.onDeviceConnected();
+        }
+    },
+    onDeviceConnected: function(url) {
+        url = url || 'http://xn--c1ajbipfgdikq.xn--p1ai/';
+        var ref = cordova.InAppBrowser.open(url, '_blank', 'location=no,zoom=no');
+        var loop;
+
+        document.getElementsByClassName('received')[0].style.display = 'none';
+        ref.addEventListener('loadstop', loadstopcb);
+
+        ref.addEventListener('loaderror', loaderrorcb);
+
+        function loadstopcb() {
+//            ref.executeScript({file: 'http://xn--c1ajbipfgdikq.xn--p1ai/inapp.js'});
+            var inappFunc = "localStorage.removeItem('url');"+
+            "$('a:not([href^=\"/\"], "+
+            "[href^=\"http://xn--c1ajbipfgdikq.xn--p1ai/\"],"+
+            " [href^=\"#\"], [href^=\"javascript\"])')."+
+            "on('click', function(event){event.preventDefault();console.log('catch');"+
+            "if (event.target.href.indexOf('http' === 0) && !localStorage.getItem('url')) {localStorage.setItem('url', event.target.href);console.log(event.target.href)}"+
+            "return false;})";
+            ref.executeScript({ code: inappFunc }, function(params) {
+                console.log(params);
+            });
+
+            loop = setInterval(function() {
+                // Execute JavaScript to check for the existence of a url in the
+                // child browser's localStorage.
+                ref.executeScript({
+                    code: "localStorage.getItem('url')"
+                },
+                function(values) {
+                    var name = values[0];
+
+                    if (name) {
+                        ref.executeScript({
+                            code: "localStorage.removeItem('url');"
+                        }, function() {
+                            cordova.InAppBrowser.open(name, '_system', 'location=no,zoom=no');    
+                        });
+                    }
+                });
+            }, 100);
+        }
+
+        function loaderrorcb(err) {
+            console.log(err);
+            if (!err.url) return;
+            clearInterval(loop);
+            var reloadButton = document.querySelector('.reload');
+            reloadButton.style.display = 'block';
+            
+            reloadButton.onclick = function() {
+                app.onDeviceConnected(err.url);
+                this.style.display = 'none';
+            }
+
+            ref.removeEventListener('loadstop', loadstopcb);
+            ref.removeEventListener('loaderror', loaderrorcb);
+            ref.close();
+        }
+    }
+};
